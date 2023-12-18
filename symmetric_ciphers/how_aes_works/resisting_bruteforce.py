a52bda0825ad87337a2999f09d786195c91a0153
Kolczan <marcin.kolacz4@gmail.com>
2023-12-18 02:40:51 +0100

    AES & Symmetric Ciphers started

---
+++ symmetric_ciphers/how_aes_works/resisting_bruteforce.py
@@ -0,0 +1,6 @@
+# It turns out that there is an attack on AES that's better than bruteforce,
+# but only slightly – it lowers the security level of AES-128 down to 126.1 bits, and hasn't been improved on for over 8 years.
+# Given the large "security margin" provided by 128 bits, and the lack of improvements despite extensive study, it's not considered a credible risk to the security of AES.
+# But yes, in a very narrow sense, it "breaks" AES.
+
+# https://en.wikipedia.org/wiki/Biclique_attack
